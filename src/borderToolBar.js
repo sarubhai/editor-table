7a8ddee3f411d866f3053a030f7535ca8cefa728
saurav mitra <saurav.karate@gmail.com>
2020-05-19 10:44:51 +0800

    first commit

---
+++ src/borderToolBar.js
@@ -0,0 +1,371 @@
+import './styles/border-toolbar.pcss'
+import svgPlusButton from './img/plus.svg'
+import svgMunusButton from './img/minus.svg'
+import {
+  create,
+  getRectOffset,
+  getCurTableFromButton,
+  debounce,
+} from './documentUtils'
+
+const CSS = {
+  highlightingLine: 'tc-toolbar',
+  hidden: 'tc-toolbar--hidden',
+  horizontalToolBar: 'tc-toolbar--hor',
+  horizontalHighlightingLine: 'tc-toolbar__shine-line--hor',
+  verticalToolBar: 'tc-toolbar--ver',
+  verticalHighlightingLine: 'tc-toolbar__shine-line--ver',
+  mask: 'tc-toolbar__mask',
+  maskAdd: 'tc-toolbar__mask--add',
+  maskDelete: 'tc-toolbar__mask--delete',
+  horizontalMask: 'tc-toolbar__mask--hor',
+  verticalMask: 'tc-toolbar__mask--ver',
+  plusButton: 'tc-toolbar__plus',
+  horizontalPlusButton: 'tc-toolbar__plus--hor',
+  verticalPlusButton: 'tc-toolbar__plus--ver',
+
+  minusButton: 'tc-toolbar__minus',
+  horizontalMinusButton: 'tc-toolbar__minus--hor',
+  verticalMinusButton: 'tc-toolbar__minus--ver',
+  area: 'tc-table__area',
+}
+
+/**
+ * An item with a menu that appears when you hover over a _table border
+ */
+class BorderToolBar {
+  /**
+   * @constructor
+   */
+  constructor() {
+    this._plusButton = this._generatePlusButton()
+    this._minusButton = this._generateMinusButton()
+    this._highlightingLine = this._generateHighlightingLine()
+    this._mask = this._generateToolBarMask()
+    this._toolbar = this._generateToolBar([
+      this._plusButton,
+      this._minusButton,
+      this._highlightingLine,
+      this._mask,
+    ])
+    // this._toolbar = this._generateToolBar([this._plusButton, this._mask]);
+  }
+
+  /**
+   * Make the entire item invisible
+   */
+  hide() {
+    this._toolbar.classList.add(CSS.hidden)
+  }
+
+  /**
+   * Make the entire item visible
+   */
+  show() {
+    this._toolbar.classList.remove(CSS.hidden)
+    this._highlightingLine.classList.remove(CSS.hidden)
+  }
+
+  /**
+   * Hide only highlightingLine
+   */
+  hideLine() {
+    this._highlightingLine.classList.add(CSS.hidden)
+  }
+
+  /**
+   * returns HTMLElement for insert in DOM
+   * @returns {HTMLElement}
+   */
+  get htmlElement() {
+    return this._toolbar
+  }
+
+  /**
+   * Generates a menu button to add rows and columns.
+   * @return {HTMLElement}
+   */
+  _generatePlusButton() {
+    const button = create('div', [CSS.plusButton])
+
+    button.innerHTML = svgPlusButton
+    button.addEventListener('click', event => {
+      event.stopPropagation()
+      const e = new CustomEvent('click', {
+        detail: { x: event.pageX, y: event.pageY },
+        bubbles: true,
+      })
+
+      this._toolbar.dispatchEvent(e)
+    })
+
+    /*
+     * button.addEventListener('mouseover', debounce((event) => {
+     *   event.stopPropagation();
+     *   this._mask.classList.add(CSS.maskAdd);
+     */
+
+    /*
+     *   this._mask.style.display = 'block';
+     * }, 300));
+     */
+
+    button.addEventListener(
+      'mouseout',
+      debounce(event => {
+        event.stopPropagation()
+        this._mask.classList.remove(CSS.maskDelete)
+        this._mask.classList.remove(CSS.maskAdd)
+        this._mask.style.display = 'none'
+      }, 300)
+    )
+
+    return button
+  }
+
+  /**
+   * Generates a menu button to delete rows and columns.
+   * @return {HTMLElement}
+   */
+  _generateMinusButton() {
+    const button = create('div', [CSS.minusButton])
+
+    button.innerHTML = svgMunusButton
+    button.style = 'transform: rotate(45deg)'
+    button.addEventListener('click', event => {
+      event.stopPropagation()
+      const e = new CustomEvent('click', {
+        detail: { x: event.pageX, y: event.pageY, action: 'minus' },
+        bubbles: true,
+      })
+
+      this._toolbar.dispatchEvent(e)
+    })
+
+    button.addEventListener(
+      'mouseout',
+      debounce(event => {
+        event.stopPropagation()
+        this._mask.classList.remove(CSS.maskDelete)
+        this._mask.classList.remove(CSS.maskAdd)
+        this._mask.style.display = 'none'
+      }, 300)
+    )
+
+    return button
+  }
+
+  /**
+   * @private
+   *
+   * Generates line which сover border of _table
+   */
+  _generateHighlightingLine() {
+    const line = create('div', [CSS.highlightingLine])
+
+    line.addEventListener('click', event => {
+      console.log('line click 禁止')
+      event.stopPropagation()
+      // const e = new CustomEvent('click', {'bubbles': true});
+
+      // this._toolbar.dispatchEvent(e);
+    })
+    return line
+  }
+
+  /**
+   * @private
+   *
+   * Generates the main component of the class
+   * @param {Element[]} children - child elements of toolbar
+   */
+  _generateToolBar(children) {
+    const bar = create('div', [CSS.hidden], null, children)
+
+    bar.addEventListener('mouseleave', event => {
+      this._recalcMousePos(event)
+    })
+
+    return bar
+  }
+
+  /**
+   * @private
+   *
+   * Generates the main component of the class
+   * @param {Element[]} children - child elements of toolbar
+   */
+  _generateToolBarMask() {
+    const mask = create('div', [CSS.mask])
+
+    return mask
+  }
+
+  /**
+   * @private
+   *
+   * Recalc mouse position when the mouse left toolbar
+   * @param {MouseEvent} event
+   */
+  _recalcMousePos(event) {
+    this.hide()
+    const area = document.elementFromPoint(event.pageX, event.pageY)
+
+    if (area !== null && area.classList.contains(CSS.area)) {
+      // eslint-disable-next-line no-undef
+      const e = new MouseEvent('mouseover', {
+        clientX: event.pageX,
+        clientY: event.pageY,
+      })
+
+      area.dispatchEvent(e)
+    }
+  }
+}
+
+/**
+ * An item with a menu that appears when you hover over a _table border horizontally
+ */
+export class HorizontalBorderToolBar extends BorderToolBar {
+  /**
+   * Creates
+   */
+  constructor() {
+    super()
+
+    this._toolbar.classList.add(CSS.horizontalToolBar)
+    this._plusButton.classList.add(CSS.horizontalPlusButton)
+    this._minusButton.classList.add(CSS.horizontalMinusButton)
+    this._highlightingLine.classList.add(CSS.horizontalHighlightingLine)
+    this._mask.classList.add(CSS.horizontalMask)
+
+    this._plusButton.addEventListener(
+      'mouseover',
+      debounce(event => {
+        event.stopPropagation()
+        this._mask.classList.add(CSS.maskAdd)
+        this._mask.style.top = '0'
+        this._mask.style.left = '0'
+
+        this._mask.style.display = 'block'
+      }, 300)
+    )
+
+    this._minusButton.addEventListener(
+      'mouseover',
+      debounce(event => {
+        event.stopPropagation()
+        const table = getCurTableFromButton(event)
+        const { topOffset, rightOffset, bottomOffset } = getRectOffset(
+          table,
+          event.target.parentElement
+        )
+
+        // if is left border OR bottom border OR top border, should add offset
+        this._mask.classList.add(CSS.maskDelete)
+
+        if (topOffset === -12 && rightOffset === 10 && bottomOffset !== 10) {
+          // console.log('is top border');
+          this._mask.style.top = '0'
+          this._mask.style.left = '0'
+        } else if (
+          topOffset !== -12 &&
+          rightOffset === 10 &&
+          bottomOffset === 10
+        ) {
+          this._mask.style.top = '-16px'
+          this._mask.style.left = '0'
+          // console.log('is hor bottom');
+        }
+        this._mask.style.display = 'block'
+      }, 300)
+    )
+  }
+
+  /**
+   * Move ToolBar to y coord
+   * @param {number} y - coord
+   */
+  showIn(y) {
+    // const halfHeight = Math.floor(Number.parseInt(window.getComputedStyle(this._toolbar).height) / 2);
+    const halfHeight = Math.floor(
+      Number.parseInt(window.getComputedStyle(this._toolbar).height)
+    )
+
+    this._toolbar.style.top = y - halfHeight + 'px'
+    this.show()
+  }
+}
+
+/**
+ * An item with a menu that appears when you hover over a _table border vertically
+ */
+export class VerticalBorderToolBar extends BorderToolBar {
+  /**
+   * Creates
+   */
+  constructor() {
+    super()
+
+    this._toolbar.classList.add(CSS.verticalToolBar)
+    this._plusButton.classList.add(CSS.verticalPlusButton)
+    this._minusButton.classList.add(CSS.verticalMinusButton)
+    this._highlightingLine.classList.add(CSS.verticalHighlightingLine)
+    this._mask.classList.add(CSS.verticalMask)
+
+    this._plusButton.addEventListener(
+      'mouseover',
+      debounce(event => {
+        event.stopPropagation()
+        this._mask.classList.add(CSS.maskAdd)
+        this._mask.style.top = '0'
+        this._mask.style.left = '0'
+
+        this._mask.style.display = 'block'
+      }, 300)
+    )
+
+    this._minusButton.addEventListener(
+      'mouseover',
+      debounce(event => {
+        event.stopPropagation()
+        const table = getCurTableFromButton(event)
+        const {
+          topOffset,
+          leftOffset,
+          rightOffset,
+          bottomOffset,
+        } = getRectOffset(table, event.target.parentElement)
+
+        this._mask.classList.add(CSS.maskDelete)
+        if (leftOffset === -12 && bottomOffset === 10) {
+          this._mask.style.left = '0'
+          // console.log('is left border');
+        } else if (
+          topOffset !== -12 &&
+          rightOffset === 10 &&
+          bottomOffset === 10
+        ) {
+          this._mask.style.left = '-16px'
+          // console.log('is ver bottom');
+        }
+
+        this._mask.style.display = 'block'
+      }, 300)
+    )
+  }
+
+  /**
+   * Move ToolBar to x coord
+   * @param {number} x - coord
+   */
+  showIn(x) {
+    // const halfWidth = Math.floor(Number.parseInt(window.getComputedStyle(this._toolbar).width) / 2);
+    const halfWidth = Math.floor(
+      Number.parseInt(window.getComputedStyle(this._toolbar).width)
+    )
+
+    this._toolbar.style.left = x - halfWidth + 'px'
+    this.show()
+  }
+}
---
+++ src/table.js
@@ -0,0 +1,292 @@
+import { create, getCoords, getSideByCoords } from './documentUtils';
+import './styles/table.pcss';
+
+const CSS = {
+  table: 'tc-table',
+  inputField: 'tc-table__inp',
+  cell: 'tc-table__cell',
+  wrapper: 'tc-table__wrap',
+  area: 'tc-table__area',
+  tdPrefix: 'td-index-'
+};
+
+/**
+ * Generates and manages _table contents.
+ */
+export class Table {
+  /**
+   * Creates
+   */
+  constructor() {
+    this._numberOfColumns = 0;
+    this._numberOfRows = 0;
+    this._element = this._createTableWrapper();
+    this._table = this._element.querySelector('table');
+
+    this._hangEvents();
+  }
+
+  /**
+   * Add column in table on index place
+   * @param {number} index - number in the array of columns, where new column to insert,-1 if insert at the end
+   */
+  addColumn(index = -1) {
+    this._numberOfColumns++;
+    /** Add cell in each row */
+    const rows = this._table.rows;
+
+    for (let i = 0; i < rows.length; i++) {
+      const cell = rows[i].insertCell(index);
+
+      /*
+       * cell.classList.add('td-cell-index' + i);
+       * console.log('add column cell: ', cell);
+       */
+
+      this._fillCell(cell);
+    }
+
+    this._rebuildCellIndex();
+  };
+
+  /**
+   * Add row in table on index place
+   * @param {number} index - number in the array of columns, where new column to insert,-1 if insert at the end
+   * @return {HTMLElement} row
+   */
+  addRow(index = -1) {
+    this._numberOfRows++;
+    const row = this._table.insertRow(index);
+
+    this._fillRow(row);
+
+    this._rebuildCellIndex();
+    return row;
+  };
+
+  /**
+   * delete coloumn
+   * @return {void}
+   */
+  deleteColumn(tdIndexClassName) {
+    const tds = this._table.querySelectorAll('.' + tdIndexClassName);
+
+    if (tds.length === 0) return false;
+
+    this._numberOfColumns -= 1;
+
+    for (let i = 0; i < tds.length; i++) {
+      tds[i].remove();
+    }
+
+    this._rebuildCellIndex();
+  }
+
+  /**
+   * delete row
+   * @return {void}
+   */
+  deleteRow(row) {
+    if (row) {
+      row.remove();
+    }
+  }
+
+  /**
+   * get html element of table
+   * @return {HTMLElement}
+   */
+  get htmlElement() {
+    return this._element;
+  }
+
+  /**
+   * get real table tag
+   * @return {HTMLElement}
+   */
+  get body() {
+    return this._table;
+  }
+
+  /**
+   * returns selected/editable cell
+   * @return {HTMLElement}
+   */
+  get selectedCell() {
+    return this._selectedCell;
+  }
+
+  /**
+   * @private
+   *
+   * Creates table structure
+   * @return {HTMLElement} tbody - where rows will be
+   */
+  _createTableWrapper() {
+    return create('div', [ CSS.wrapper ], null, [ create('table', [ CSS.table ]) ]);
+  }
+
+  /**
+   * @private
+   *
+   * Create editable area of cell
+   * @return {HTMLElement} - the area
+   */
+  _createContenteditableArea() {
+    return create('div', [ CSS.inputField ], { contenteditable: 'true' });
+  }
+
+  /**
+   * @private
+   *
+   * Fills the empty cell of the editable area
+   * @param {HTMLElement} cell - empty cell
+   */
+  _fillCell(cell) {
+    cell.classList.add(CSS.cell);
+    // if (columnIndex !== undefined) cell.classList.add(CSS.tdPrefix + columnIndex);
+
+    const content = this._createContenteditableArea();
+
+    cell.appendChild(create('div', [ CSS.area ], null, [ content ]));
+  }
+
+  /**
+   * @private
+   *
+   * Fills the empty row with cells  in the size of numberOfColumns
+   * @param row = the empty row
+   */
+  _fillRow(row) {
+    for (let i = 0; i < this._numberOfColumns; i++) {
+      const cell = row.insertCell();
+
+      this._fillCell(cell);
+    }
+  }
+
+  /**
+   * @private
+   *
+   * hang necessary events
+   */
+  _rebuildCellIndex() {
+    const tbody = this._table.querySelector('tbody');
+
+    setTimeout(() => {
+      for (let rowIdx = 0; rowIdx < tbody.childNodes.length; rowIdx++) {
+        const tr = tbody.childNodes[rowIdx];
+
+        for (let rdIdx = 0; rdIdx < tr.childNodes.length; rdIdx++) {
+          tr.childNodes[rdIdx].className = 'tc-table__cell cell-index-' + rdIdx;
+        }
+      }
+    }, 100);
+  }
+
+  /**
+   * @private
+   *
+   * hang necessary events
+   */
+  _hangEvents() {
+    this._table.addEventListener('focus', (event) => {
+      this._focusEditField(event);
+    }, true);
+
+    this._table.addEventListener('blur', (event) => {
+      this._blurEditField(event);
+    }, true);
+
+    this._table.addEventListener('keydown', (event) => {
+      this._pressedEnterInEditField(event);
+    });
+
+    this._table.addEventListener('click', (event) => {
+      this._clickedOnCell(event);
+    });
+
+    this._table.addEventListener('mouseover', (event) => {
+      this._mouseEnterInDetectArea(event);
+      event.stopPropagation();
+    }, true);
+  }
+
+  /**
+   * @private
+   *
+   * When you focus on an editable field, remembers the cell
+   * @param {FocusEvent} event
+   */
+  _focusEditField(event) {
+    if (!event.target.classList.contains(CSS.inputField)) {
+      return;
+    }
+    this._selectedCell = event.target.closest('.' + CSS.cell);
+  }
+
+  /**
+   * @private
+   *
+   * When you blur on an editable field, remembers the cell
+   * @param {FocusEvent} event
+   */
+  _blurEditField(event) {
+    if (!event.target.classList.contains(CSS.inputField)) {
+      return;
+    }
+    this._selectedCell = null;
+  }
+
+  /**
+   * @private
+   *
+   * When enter is pressed when editing a field
+   * @param {KeyboardEvent} event
+   */
+  _pressedEnterInEditField(event) {
+    if (!event.target.classList.contains(CSS.inputField)) {
+      return;
+    }
+    if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault();
+    }
+  }
+
+  /**
+   * @private
+   *
+   * When clicking on a cell
+   * @param {MouseEvent} event
+   */
+  _clickedOnCell(event) {
+    if (!event.target.classList.contains(CSS.cell)) {
+      return;
+    }
+    const content = event.target.querySelector('.' + CSS.inputField);
+
+    content.focus();
+  }
+
+  /**
+   * @private
+   *
+   * When the mouse enters the detection area
+   * @param {MouseEvent} event
+   */
+  _mouseEnterInDetectArea(event) {
+    if (!event.target.classList.contains(CSS.area)) {
+      return;
+    }
+
+    const coordsCell = getCoords(event.target.closest('TD'));
+    const side = getSideByCoords(coordsCell, event.pageX, event.pageY);
+
+    event.target.dispatchEvent(new CustomEvent('mouseInActivatingArea', {
+      detail: {
+        side: side
+      },
+      bubbles: true
+    }));
+  }
+}
